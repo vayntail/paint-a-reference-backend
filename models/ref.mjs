1d8e7ca400f8ebdf56fd35060002a79e91392384
vayntail <113714157+vayntail@users.noreply.github.com>
2025-01-10 09:09:10 -0500

    model changes

--- models/ref.mjs
+++ models/ref.mjs
@@ -4,12 +4,11 @@ const refSchema = new mongoose.Schema(
   {
     imageUrl: { type: String, required: true },
     uploadDate: { type: Date, default: Date.now },
-    type: { type: String, enum: ["Photo", "Screencap"], required: true },
-    screencapCategory: {
+    type: {
       type: String,
-      enum: ["Film", "Game", "Animation"],
-      required: false,
-    }, // only for screencaps
+      enum: ["Photo", "Film", "Game", "Animation"],
+      required: true,
+    },
     tags: [{ type: String }],
     uploadedBy: {
       type: mongoose.Schema.Types.ObjectId,
